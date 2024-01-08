b96c17f033ff9d83452383af93a3b00340705481
Artur K <a.kulig19@gmail.com>
2024-01-08 20:16:11 +0100

    no message

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "QLQ ART-gra-szkoleniowa-1";
+const contentToCache = [
+    "Build/gra-szkoleniowa-pf-build.loader.js",
+    "Build/gra-szkoleniowa-pf-build.framework.js",
+    "Build/gra-szkoleniowa-pf-build.data",
+    "Build/gra-szkoleniowa-pf-build.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
